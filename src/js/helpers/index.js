6270b1e74551d0ce08f8e3b6f99682f839568992
Sergey Fomenko <sfom_rabota@mail.ru>
2023-01-27 14:26:12 +0300

    feat: menu select language

--- src/js/helpers/index.js
+++ src/js/helpers/index.js
@@ -20,17 +20,21 @@ async function WebpIsSupported() {
 function init() {
 	const hamburger = document.querySelector('#hamburger');
 	const cross = document.querySelector('#close-menu');
+	const selectLanguage = document.querySelector('#select-languages');
 	attachEvents({
 		hamburger,
 		cross,
+		selectLanguage,
 	});
 }
 
 function attachEvents(objElements) {
-	const { hamburger, cross } = objElements;
+	const { hamburger, cross, selectLanguage } = objElements;
 
 	hamburger.addEventListener('click', openMenu);
 	cross.addEventListener('click', closeMenu);
+
+	selectLanguage.addEventListener('click', handleSelectLanguage);
 }
 
 function openMenu() {
@@ -39,6 +43,35 @@ function openMenu() {
 
 function closeMenu() {
 	document.querySelector('#menu').classList.remove('menu-active');
+}
+
+function handleSelectLanguage(event) {
+	const menuSelectLanguage = createMenuSelectLanguage();
+	setInDom(event.target.parentElement, menuSelectLanguage);
+}
+
+function createMenuSelectLanguage() {
+	const div = document.createElement('div');
+	div.innerHTML = `<div class="menu-language__arrow"></div> 
+        <ul class="menu-language__list">
+            <li class="menu-language__item"><a class="menu-language__link" id="Russian">Russian</a></li>
+            <li class="menu-language__item"><a class="menu-language__link" id="English">English</a></li>
+        </ul>`;
+	div.classList.add('menu-language');
+	return div;
+}
+
+function attachLanguageEvents(menuSelectLanguage) {
+	const languages = menuSelectLanguage.querySelectorAll(
+		'.menu-language__link'
+	);
+	languages.forEach((language) => {
+		//language.addEventListener
+	});
+}
+
+function setInDom(parentElement, element) {
+	parentElement.append(element);
 }
 
 init();
