6d621f2d1f2d19042f01f63d02fdcf3c1a0583dd
Sergey Fomenko <sfom_rabota@mail.ru>
2023-01-28 14:32:52 +0300

    feat: add click menu languages

--- src/js/helpers/index.js
+++ src/js/helpers/index.js
@@ -17,24 +17,30 @@ async function WebpIsSupported() {
 	}
 })();
 
-function init() {
+(function init() {
 	const hamburger = document.querySelector('#hamburger');
 	const cross = document.querySelector('#close-menu');
 	const selectLanguage = document.querySelector('#select-languages');
+	const selectLanguageMobile = document.querySelector(
+		'#select-languages-mobile'
+	);
 	attachEvents({
 		hamburger,
 		cross,
 		selectLanguage,
+		selectLanguageMobile,
 	});
-}
+})();
 
 function attachEvents(objElements) {
-	const { hamburger, cross, selectLanguage } = objElements;
+	const { hamburger, cross, selectLanguage, selectLanguageMobile } =
+		objElements;
 
 	hamburger.addEventListener('click', openMenu);
 	cross.addEventListener('click', closeMenu);
 
-	selectLanguage.addEventListener('click', handleSelectLanguage);
+	selectLanguage.addEventListener('click', openSelectLanguage);
+	selectLanguageMobile.addEventListener('click', openSelectLanguage);
 }
 
 function openMenu() {
@@ -45,33 +51,59 @@ function closeMenu() {
 	document.querySelector('#menu').classList.remove('menu-active');
 }
 
-function handleSelectLanguage(event) {
-	const menuSelectLanguage = createMenuSelectLanguage();
-	setInDom(event.target.parentElement, menuSelectLanguage);
+function openSelectLanguage(event) {
+	const boxControl = event.target.parentElement.parentElement;
+	const menuOrHeader = checkClickSelectLanguage(boxControl);
+	if (!boxControl.lastChild.classList?.value.includes('menu-language')) {
+		const menuSelectLanguage = createMenuSelectLanguage(menuOrHeader);
+		attachMenuLanguageEvents();
+		setInDom(boxControl, menuSelectLanguage);
+	}
 }
 
-function createMenuSelectLanguage() {
+function checkClickSelectLanguage(boxControl) {
+	console.log(boxControl.classList.value);
+	if (boxControl.classList.value.includes('header')) {
+		return 'desktop';
+	} else {
+		return 'mobile';
+	}
+}
+
+function createMenuSelectLanguage(menuOrHeader) {
 	const div = document.createElement('div');
 	div.innerHTML = `<div class="menu-language__arrow"></div> 
         <ul class="menu-language__list">
             <li class="menu-language__item"><a class="menu-language__link" id="Russian">Russian</a></li>
             <li class="menu-language__item"><a class="menu-language__link" id="English">English</a></li>
         </ul>`;
-	div.classList.add('menu-language');
+	div.classList.add(`menu-language`, menuOrHeader);
 	return div;
 }
 
-function attachLanguageEvents(menuSelectLanguage) {
-	const languages = menuSelectLanguage.querySelectorAll(
-		'.menu-language__link'
-	);
-	languages.forEach((language) => {
-		//language.addEventListener
-	});
+function attachMenuLanguageEvents() {
+	document.addEventListener('click', closeMenuSelectLanguage);
+}
+
+function closeMenuSelectLanguage(event) {
+	const clickOutMenu =
+		event.target.classList.value !== 'menu-language__list' &&
+		event.target.classList.value !== 'select-languages__languages';
+	if (clickOutMenu) {
+		deleteMenuSelectLanguage();
+	}
+}
+
+function deleteMenuSelectLanguage() {
+	const menuLanguage = document.querySelector('.menu-language');
+	detachMenuLanguageEvents();
+	menuLanguage.remove();
+}
+
+function detachMenuLanguageEvents() {
+	document.removeEventListener('click', closeMenuSelectLanguage);
 }
 
 function setInDom(parentElement, element) {
 	parentElement.append(element);
 }
-
-init();
